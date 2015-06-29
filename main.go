f2801bebfa0243b485ccda7c517e9ef46fba0331
Carlos Ignacio Pérez Sechi <external.z21csech@decathlon.com>
2015-06-29 12:32:50 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,56 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"github.com/seccijr/quinto/web/handler"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"regexp"
+)
+
+var (
+	addr = flag.Bool("addr", false, "find open address and print to final-port.txt")
+)
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", handler.Index)
+	http.HandleFunc("/view/", makeHandler(handler.View))
+	http.HandleFunc("/edit/", makeHandler(handler.Edit))
+	http.HandleFunc("/save/", makeHandler(handler.Save))
+
+	if *addr {
+		l, err := net.Listen("tcp", "127.0.0.1:0")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = ioutil.WriteFile("final-port.txt", []byte(l.Addr().String()), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := &http.Server{}
+		s.Serve(l)
+		return
+	}
+
+	http.ListenAndServe(":8080", nil)
+}
