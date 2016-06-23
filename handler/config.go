51cb4d03eb367c30794dd868dfa87b6cbb4d58df
Carlos I. Pérez Sechi <external.z21csech@decathlon.com>
2016-06-23 17:35:59 +0200

    Using public prefix in statics

--- handler/config.go
+++ handler/config.go
@@ -12,11 +12,8 @@ func Router(i18n service.I18n, ad service.Ad) *mux.Router {
 	root := environment.Root()
 	r := mux.NewRouter()
 
-	cssh := http.StripPrefix("/css/", http.FileServer(http.Dir(path.Join(root, "public/css/"))))
-	r.PathPrefix("/css/").Handler(cssh)
-
-	vendorh := http.StripPrefix("/vendor/", http.FileServer(http.Dir(path.Join(root, "public/vendor/"))))
-	r.PathPrefix("/vendor/").Handler(vendorh)
+	publicHandler := http.StripPrefix("/public/", http.FileServer(http.Dir(path.Join(root, "public/"))))
+	r.PathPrefix("/public/").Handler(publicHandler)
 
 	home := NewHome(i18n, ad)
 	r.HandleFunc("/", home.Index)
