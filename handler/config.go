cd6abc93424b7e3ac19d1a13f2a649c84255a454
Carlos I. Pérez Sechi <external.z21csech@decathlon.com>
2016-06-23 17:28:05 +0200

    Adding golang static routes to css

--- handler/config.go
+++ handler/config.go
@@ -3,10 +3,20 @@ package handler
 import (
 	"github.com/gorilla/mux"
 	"github.com/seccijr/quintoweb/service"
+	"net/http"
+	"github.com/seccijr/quintoweb/environment"
+	"path"
 )
 
 func Router(i18n service.I18n, ad service.Ad) *mux.Router {
+	root := environment.Root()
 	r := mux.NewRouter()
+
+	cssh := http.StripPrefix("/css/", http.FileServer(http.Dir(path.Join(root, "public/css/"))))
+	r.PathPrefix("/css/").Handler(cssh)
+
+	vendorh := http.StripPrefix("/vendor/", http.FileServer(http.Dir(path.Join(root, "public/vendor/"))))
+	r.PathPrefix("/vendor/").Handler(vendorh)
 
 	home := NewHome(i18n, ad)
 	r.HandleFunc("/", home.Index)
